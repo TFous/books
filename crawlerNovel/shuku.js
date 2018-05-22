64870d313dbd3e011bd650e9ff21c79462575126
Teng <475984203@qq.com>
2018-05-22 16:54:30 +0800

    123

--- crawlerNovel/shuku.js
+++ crawlerNovel/shuku.js
@@ -22,20 +22,24 @@ async function getBooks(url) {
     return new Promise( (resolve, reject) =>{
         request
             .get(url)
-            .charset('gbk')
+            // .charset('gbk')
             .end((err, res) => {
                 if (err) {
                     return next(err);
                 }
                 let items = []
                 var $ = cheerio.load(res.text);
-                let lists = $('.ranking_general .articlegeneral')
+                let tips = $('#alist h3').text().split('List')[1].split('小说最新列表')[0]
+                let lists = $('#alistbox')
                 lists.each(function (idx, element) {
-                    var a = $(element).find('a');
-                    var author = $(element).find('.p3').text();
+                    var link = $(element).find('.title h2 a');
+                    var author = $(element).find('.title span').text().split('作者：')[1];
+                    var intro = $(element).find('.intro').text();
                     items.push({
-                        href: a.attr('href'),
-                        name: a.text(),
+                        intro: intro,
+                        tips: tips,
+                        href: 'http://www.56shuku.org' + link.attr('href'),
+                        name: link.text(),
                         author: author,
                     })
                 })
@@ -43,9 +47,8 @@ async function getBooks(url) {
                 // return items
             })
     })
-
 }
-let url = 'https://m.biquge.com.tw/wapsort/1_1.html'
+let url = 'http://www.56shuku.org/leibie9_1.shtml'
 getBooks(url).then(function (item) {
     console.log(item)
 })
